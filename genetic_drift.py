25d312b6d165f25a4b96b89e9a9732b78a3561dd
Henrietta Holze <szr214@alumni.ku.dk>
2021-02-17 20:27:45 +0100

    add server

--- genetic_drift.py
+++ genetic_drift.py
@@ -243,5 +243,6 @@ def update_plot(init_p, init_N, init_n_gen, init_n_pop):
 
 
 if __name__ == "__main__":
-    app.run_server(host="127.0.0.1", port=8050, debug=True)
+    # app.run_server(host="127.0.0.1", port=8050, debug=True)
+    app.run_server(debug=True)
 
